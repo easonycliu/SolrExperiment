48b5af1ce5898d2bf3f1b5175179364783b2f7fd
liuyc <liuyicheng1515@sjtu.edu.cn>
2024-03-29 10:16:41 +0800

    add warmup script for solr, not use by cases yet

---
+++ scripts/warmup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -m
+client_num=5
+exp_duration=30
+indices=big
+
+for i in $(seq 1 1 $client_num); do
+    python microbenchmark/test_multiclient_search.py /dev/null $indices /dev/null &
+    sleep 0.1
+done
+
+sleep 10
+
+for j in $(seq 1 1 $exp_duration); do
+    kill -10 $(ps | grep python | awk '{print $1}')
+    sleep 1
+done
+
+kill -2 $(ps | grep python | awk '{print $1}')
